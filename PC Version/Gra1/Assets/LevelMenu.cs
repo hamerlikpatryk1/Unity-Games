ededfde58df113b7205ddf3f45f6e0ee8ba33c92
hamerlikpatryk1 <hamerlikpatryk1@gmail.com>
2021-01-24 21:31:14 +0100

    Initial to PC branch
    Game + Multi + Chat

---
+++ PC Version/Gra1/Assets/LevelMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelMenu : MonoBehaviour
+{
+    public void PlayLvl1()
+    {
+        SceneManager.LoadScene("Level 1"); ;
+    }
+    public void PlayLvl2()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
